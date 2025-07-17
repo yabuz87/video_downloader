9778e742b36ae53813d1c56113d4b1b50edb45fc
yabets <jabezsolomonz@gmail.com>
2025-07-17 23:24:42 +0300

    added the webfrontend and the mobile ui directories

---
+++ mobile/app/index.tsx
@@ -0,0 +1,17 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        backgroundColor:"lightblue",
+        alignItems: "center",
+      }}
+    ><View style={{width:200,height:200,backgroundColor:"lightblue"}}>
+      <Text>hey there 🤚🏾</Text>
+      </View>
+    </View>
+  );
+}
